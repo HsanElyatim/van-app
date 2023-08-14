2f224576fc1cab1a2fb5275adf00e5e55f3301c4
HsanElyatim <lytaiem.hsan@gmail.com>
2023-08-14 13:38:20 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,6 @@ function App() {
           <Link to="">Vans</Link>
         </nav>
       </header>
-        
       <Routes>
         <Route path='/' element={<Home  />} />
         <Route path='/about' element={<About />} />
