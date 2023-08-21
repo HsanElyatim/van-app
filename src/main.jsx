2044b5ac50f459384940f6dbc6625474a4c2a996
HsanElyatim <lytaiem.hsan@gmail.com>
2023-08-21 14:18:51 +0100

    Added vans list page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import "./index.css"
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
 import "./server"
 
 function App() {
@@ -13,13 +14,14 @@ function App() {
         <Link className="site-logo" to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
-          <Link to="">Vans</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
 
       <Routes>
         <Route path='/' element={<Home  />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans/>}></Route>
       </Routes>
     </BrowserRouter>
   )
