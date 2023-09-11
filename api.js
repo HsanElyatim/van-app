4ede8643239b32e67436d30406292b9cf4dbb1da
HsanElyatim <lytaiem.hsan@gmail.com>
2023-09-11 16:39:56 +0100

    final

--- api.js
+++ api.js
@@ -1,28 +1,54 @@
 import { initializeApp } from "firebase/app";
-import {
-    getFirestore,
-    collection,
-    doc,
-    getDocs,
-    getDoc,
-    query,
-    where
-} from "firebase/firestore/lite"
+import { getFirestore, 
+    collection, 
+    doc, 
+    getDocs, 
+    getDoc, 
+    setDoc, 
+    query, 
+    where } 
+from "firebase/firestore/lite"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBMFY6XNtM_nvlupc95FY9NHpiH1bnS33Y",
-    authDomain: "vanlife-6b5ff.firebaseapp.com",
-    projectId: "vanlife-6b5ff",
-    storageBucket: "vanlife-6b5ff.appspot.com",
-    messagingSenderId: "157717661274",
-    appId: "1:157717661274:web:f66a6d1940ec28fb2a3a63"
+  apiKey: "AIzaSyBMFY6XNtM_nvlupc95FY9NHpiH1bnS33Y",
+  authDomain: "vanlife-6b5ff.firebaseapp.com",
+  projectId: "vanlife-6b5ff",
+  storageBucket: "vanlife-6b5ff.appspot.com",
+  messagingSenderId: "157717661274",
+  appId: "1:157717661274:web:f66a6d1940ec28fb2a3a63"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
+// Preparing the data
 const vansCollectionRef = collection(db, "vans")
+/* export async function populateDB(){
+    await setDoc(doc(vansCollectionRef), { id: "1", name: "Modest Explorer", price: 60, description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", type: "simple", hostId: "123" })
+    await setDoc(doc(vansCollectionRef), { id: "2", name: "Beach Bum", price: 80, description: "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/beach-bum.png", type: "rugged", hostId: "123" })
+    await setDoc(doc(vansCollectionRef), { id: "3", name: "Reliable Red", price: 100, description: "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/reliable-red.png", type: "luxury", hostId: "456" })
+    await setDoc(doc(vansCollectionRef), { id: "4", name: "Dreamfinder", price: 65, description: "Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/dreamfinder.png", type: "simple", hostId: "789" })
+    await setDoc(doc(vansCollectionRef), { id: "5", name: "The Cruiser", price: 120, description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/the-cruiser.png", type: "luxury", hostId: "789" })
+    await setDoc(doc(vansCollectionRef), { id: "6", name: "Green Wonder", price: 70, description: "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", type: "rugged", hostId: "123" })
+    await setDoc(doc(vansCollectionRef), { id: "123", email: "b@b.com", password: "p123", name: "Bob" })
+} */
+    
 
+// API fetching functions
+/* export async function getVans(id) {
+    const url = id ? `/api/vans/${id}` : "/api/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+} */
 export async function getVans() {
     const querySnapshot = await getDocs(vansCollectionRef)
     const dataArr = querySnapshot.docs.map(doc => ({
@@ -35,12 +61,25 @@ export async function getVans() {
 export async function getVan(id) {
     const docRef = doc(db, "vans", id)
     const vanSnapshot = await getDoc(docRef)
-    return {
+    return({
         ...vanSnapshot.data(),
         id: vanSnapshot.id
-    }
+    })
 }
 
+/* export async function getHostVans(id) {
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+} */
 export async function getHostVans() {
     const q = query(vansCollectionRef, where("hostId", "==", "123"))
     const querySnapshot = await getDocs(q)
